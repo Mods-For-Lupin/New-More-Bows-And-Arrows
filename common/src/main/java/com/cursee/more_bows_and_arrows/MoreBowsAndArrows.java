22059a46631deea71136dd1426fe0e837c611959
jason13official <jason13official@gmail.com>
2025-11-19 16:49:05 -0500

    feat: common id method

--- common/src/main/java/com/cursee/more_bows_and_arrows/MoreBowsAndArrows.java
+++ common/src/main/java/com/cursee/more_bows_and_arrows/MoreBowsAndArrows.java
@@ -1,5 +1,6 @@
 package com.cursee.more_bows_and_arrows;
 
+import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,5 +9,9 @@ public class MoreBowsAndArrows {
   public static final Logger LOG = LoggerFactory.getLogger(Constants.MOD_NAME);
 
   public static void init() {
+  }
+
+  public static ResourceLocation identifier(String path) {
+    return new ResourceLocation(Constants.MOD_ID, path);
   }
 }
